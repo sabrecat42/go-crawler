363f790ff943e9ae1732374712396aa466cc5233
Adrian Josan <josan.adrian11@gmail.com>
2025-05-13 11:51:20 +0200

    Implement basic version storing cookies for between requests

--- crawler.go
+++ crawler.go
@@ -2,53 +2,69 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"net/http/cookiejar"
 
 	"github.com/gocolly/colly"
 )
 
+// initialize a map to store visited URLs
+var visitedurls = make(map[string]bool)
+
 func main() {
+	// define the seed URL
+	seedurl := "https://www.scrapingcourse.com/ecommerce/"
 
-	file, err := os.Create("output.txt")
-	if err != nil {
-		log.Fatalf("Failed to create file: %s", err)
-	}
-	defer file.Close()
+	// call the crawl function
+	crawl(seedurl, 5)
+}
 
-	urlToScrape := "https://news.ycombinator.com/item?id=17203903"
+func crawl(currenturl string, maxdepth int) {
+	// instantiate  a new collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+		colly.MaxDepth(maxdepth),
+	)
 
-	c := colly.NewCollector(colly.AllowedDomains("news.ycombinator.com"))
-
+	// add an OnRequest callback to track progress
 	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("Accept-Language", "ru-RU")
+		// set custom headers
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (compatible; Colly/2.1; +https://github.com/gocolly/colly)")
+		fmt.Println("Crawling", r.URL)
 	})
 
-	c.OnHTML("td.title span.titleline", func(h *colly.HTMLElement) {
-		// fmt.Println("Title:", h.Text)
-		line := fmt.Sprintf("Title: %s\n", h.Text)
-		fmt.Print(line)
-		_, err := file.WriteString(line)
-		if err != nil {
-			log.Printf("Failed to write title: %s", err)
+	// manage cookies
+	cookiesJar, _ := cookiejar.New(nil)
+	c.SetCookieJar(cookiesJar)
+
+	// extract and log the page title
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println("Page Title:", e.Text)
+	})
+
+	// ----- find and visit all links ---- //
+	// select the href attribute of all anchor tags
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// get absolute URL
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		// check if current URL has already been visited
+		if link != "" && !visitedurls[link] {
+			// add current URL to visitedURLs
+			visitedurls[link] = true
+			fmt.Println("Found link:", link)
+			// visit current URL
+			e.Request.Visit(link)
 		}
 	})
 
-	c.OnHTML("div.comment div.commtext", func(h *colly.HTMLElement) {
-		// selection = h.DOM
-		// fmt.Println("Comment:", h.Text)
-		line := fmt.Sprintf("Comment: %s\n\n", h.Text)
-		fmt.Print(line)
-		_, err := file.WriteString(line)
-		if err != nil {
-			log.Printf("Failed to write comment: %s", err)
-		}
+	// handle request errors
+	c.OnError(func(e *colly.Response, err error) {
+		fmt.Println("Request URL:", e.Request.URL, "failed with response:", e, "\nError:", err)
 	})
 
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Printf("Error while scrapping: %s\n", err.Error())
-	})
-
-	c.Visit(urlToScrape)
+	// visit the seed URL
+	err := c.Visit(currenturl)
+	if err != nil {
+		fmt.Println("Error visiting page:", err)
+	}
 
 }
